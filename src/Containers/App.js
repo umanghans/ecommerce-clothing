fac56174ee3aacf1140388f95c0905b3931ab00f
umanghans <umang.hans1994@gmail.com>
2020-07-13 15:20:15 -0400

    first commit

---
+++ src/Containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';import './App.css';
+import HomePage from '../Components/Homepage/Homepage';
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
